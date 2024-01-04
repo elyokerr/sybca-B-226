e28f7c24acb67b3dc65d113a4ca0485388cdc9d6
samuel demelo <samueldemelo0832@gmail.com>
2024-01-04 09:39:11 +0530

    chipi chipi chapa chapa

---
+++ SybcaB4Project/src/test/pakageB4Sybca.java
@@ -0,0 +1,12 @@
+package test;
+
+public class pakageB4Sybca {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("GitHub Hello World");
+	}
+	
+
+}
