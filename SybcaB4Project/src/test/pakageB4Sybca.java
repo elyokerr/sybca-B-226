ac8fb5252dc31e5c6e497202047fbb1e40e962b9
samuel demelo <samueldemelo0832@gmail.com>
2024-01-11 10:30:50 +0530

    add 2 nums commit stage

--- SybcaB4Project/src/test/pakageB4Sybca.java
+++ SybcaB4Project/src/test/pakageB4Sybca.java
@@ -1,12 +1,23 @@
 package test;
 
+import java.util.Scanner;
+
 public class pakageB4Sybca {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+        
 
-		System.out.println("GitHub Hello World");
-	}
+		int num1,num2,sum;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter 1st num: ");
+		num1=sc.nextInt();
+		System.out.println("Enter 2nd num: ");
+		num2=sc.nextInt();
+		sum=num1+num2;
+		System.out.println("The addition is: "+sum);
+	
 	
 
+  }
 }
