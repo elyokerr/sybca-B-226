8340502f086a9c840dc510bbb22a3f0427f06550
samuel demelo <samueldemelo0832@gmail.com>
2024-01-11 10:57:52 +0530

    merger addition to subtraction

--- SybcaB4Project/src/test/pakageB4Sybca.java
+++ SybcaB4Project/src/test/pakageB4Sybca.java
@@ -7,15 +7,15 @@ public class pakageB4Sybca {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         
-
-		int num1,num2,sum;
+//program to subtract 2 numbers.
+		int num1,num2,minus;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter 1st num: ");
 		num1=sc.nextInt();
 		System.out.println("Enter 2nd num: ");
 		num2=sc.nextInt();
-		sum=num1+num2;
-		System.out.println("The addition is: "+sum);
+		minus=num1-num2;
+		System.out.println("The addition is: "+minus);
 	
 	
 
